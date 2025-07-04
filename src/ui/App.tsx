8a99f9134bf7bb32d73cf2ee9e53a59278a2e329
damian-pebe <damian.pebe@gmail.com>
2025-07-03 19:34:10 -0600

    add(i18): integrate i18

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,10 +1,24 @@
+import { useTranslation } from "react-i18next";
+import ToggleTranslate from "./src/components/toggleTranslate";
+
+const Welcome = () => {
+  const { t } = useTranslation("common");
+
+  return <h1>{t("welcome")}</h1>;
+};
+
+const Logout = () => {
+  const { t } = useTranslation("common");
+
+  return <p>{t("logout")}</p>;
+};
+
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold underline">
-        Hello OrB Z + Tailwind Css
-      </h1>
-      <p>This file will be only for routing</p>
+      <Welcome />
+      <Logout />
+      <ToggleTranslate />
     </div>
   );
 }
