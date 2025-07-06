3d08f779ead5bba152e64f890f9e2d84733d61a8
damian-pebe <damian.pebe@gmail.com>
2025-07-06 11:26:17 -0600

    add(window-electron): set up off electron mmain window appearance and min configs

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,27 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
+import options from "./mainWindowConfig.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  
+
+  const mainWindow = new BrowserWindow(options);
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.setAutoHideMenuBar(false);
+
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
 });
+
+//TODO because i remove the menu
+
+// Build your own title bar UI using HTML/CSS/React inside your app.
+
+// Add buttons for close, minimize, maximize/restore.
+
+// Hook them to Electron window APIs using ipcRenderer or @electron/remote.
+
