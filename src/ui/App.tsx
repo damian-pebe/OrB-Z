bcb6e78f1e9b34d4e3c22a615fbeb20adaf63abf
damian-pebe <damian.pebe@gmail.com>
2025-07-05 22:58:51 -0600

    add(hero-i18n): change hero for es-en config

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,23 +1,18 @@
 import { useTranslation } from "react-i18next";
 import ToggleTranslate from "./src/components/toggleTranslate";
 
-const Welcome = () => {
-  const { t } = useTranslation("common");
+// const Welcome = () =>
+//   const { t } = useTranslation("common");
 
-  return <h1>{t("welcome")}</h1>;
-};
-
-const Logout = () => {
-  const { t } = useTranslation("common");
-
-  return <p>{t("logout")}</p>;
-};
+//   return <h1>{t("welcome")}</h1>;
+// }; // Another way to use the Translation hook
 
 function App() {
+  const { t } = useTranslation("common");
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <Welcome />
-      <Logout />
+    <div className="flex flex-col items-center justify-center h-screen font-righteous">
+      <h1>{t("welcome")}</h1>
+      <p>{t("logout")}</p>
       <ToggleTranslate />
     </div>
   );
---
+++ src/ui/src/components/fontsView.tsx
@@ -0,0 +1,21 @@
+export default function FontsView() {
+  return (
+    <div className="text-3xl">
+      <p className="font-abril">font</p>
+      <p className="font-cormorant">font</p>
+      <p className="font-dancing">font</p>
+      <p className="font-greatvibes">font</p>
+      <p className="font-lobster">font</p>
+      <p className="font-nunito">font</p>
+      <p className="font-pacifico">font</p>
+      <p className="font-poiret">font</p>
+      <p className="font-poppins">font</p>
+      <p className="font-righteous">font</p>
+      <p className="font-spacegrotesk">font</p>
+      <p className="font-unica">font</p>
+    </div>
+  );
+}
+
+
+
