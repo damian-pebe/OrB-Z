f632892f0371e6ebaf11f575139fcbefbf7cd7f6
damian-pebe <damian.pebe@gmail.com>
2025-07-09 08:42:46 -0600

    feat(react-ui): add TanStack wrapper on main.tsx react app

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode, Suspense } from "react";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./i18n";
-import './global.css'
-import App from './App'
+import "./global.css";
+import App from "./App";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense fallback={<div>Loading translations...</div>}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Suspense>
   </StrictMode>
 );
