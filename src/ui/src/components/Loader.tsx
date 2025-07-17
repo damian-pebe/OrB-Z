d2131feab5104b5e508999358b49d2a10e86ccf0
damian-pebe <damian.pebe@gmail.com>
2025-07-16 19:47:31 -0600

    feat(routing): add routing for app and background grids for landing

---
+++ src/ui/src/components/Loader.tsx
@@ -0,0 +1,16 @@
+export default function Loader() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+      }}
+    >
+      Loading...
+    </div>
+  );
+}
